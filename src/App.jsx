e0e71a5b3bd856df579daa58a27bb38b9549c3e2
hidahid <ahid.abdurrahman.saleh@gmail.com>
2023-05-23 01:03:36 +0700

    create Todos and TodoItem component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todos from './components/Todos'
 
 // function App() {
 //   const [count, setCount] = useState(0)
@@ -71,9 +72,7 @@ function App() {
   return (
     <div>
       <h1>My Todo List</h1>
-      {todos.map((todo) => {
-        return <p key={todo.id}>{todo.title}</p>
-      })}
+      <Todos todos={todos} />
     </div>
   )
 }
